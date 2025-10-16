0a046e3d02ed838500b2bf9c7896ea126825022f
Policki <eenej31@gmail.com>
2025-10-16 09:54:20 +0200

    Update Program.cs
    Dodal komentarje pri vsaki metodi.

--- Vaja6/Program.cs
+++ Vaja6/Program.cs
@@ -10,21 +10,21 @@ namespace vaja6 {
         public string u_geslo;
         public static int stIg = 1;
 
-        public Igralec() {
+        public Igralec() { //Ustvari novega igralca z unikatnim ID-jem, praznim uporabniškim imenom in geslom. Poveča števec igralcev.
             u_ime = "";
             u_geslo = "";
             id = stIg;
             stIg++;
         }
 
-        public int Id {
+        public int Id { //Vrne ID igralca
             get
             {
                 return id;
             }
         }
 
-        public string U_ime {
+        public string U_ime { //Dovoli prebrati in nastaviti uporabniško ime igralca. Pri nastavljanju odstrani morebitne presledke na začetku in koncu.
             get
             {
                 return u_ime;
@@ -35,7 +35,7 @@ namespace vaja6 {
             }
         }
 
-        public string U_geslo
+        public string U_geslo //Dovoli prebrati in nastaviti geslo igralca. Pri nastavljanju odstrani presledke na začetku in koncu.
         {
             get
             {
@@ -47,7 +47,7 @@ namespace vaja6 {
             }
         }
 
-        public static int StIg
+        public static int StIg //Statistična lastnost, ki vrne trenutno vrednost števca ustvarjenih igralcev.
         {
             get { return stIg; }
         }
